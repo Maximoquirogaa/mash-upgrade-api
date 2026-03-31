f9ac5bb5f5072aa4a68a3a89ba56dd29a3d74447
Maximoquirogaa <maximoquirogajc@gmail.com>
2026-03-31 12:30:57 -0300

    Refactor session.py to remove commented code

--- database/session.py
+++ database/session.py
@@ -2,15 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# # En producción, esta URL se lee de un archivo .env
-# # Estructura: postgresql://[user]:[password]@[host]:[port]/[db_name]
 DATABASE_URL = "postgresql://postgres:[YOUR-PASSWORD]@db.eslzcyrfedgnboqmjayv.supabase.co:5432/postgres"
 
 class DatabaseManager:
     def __init__(self, connection_url: str):
-        # # El 'engine' es el corazón de la conexión
         self.engine = create_engine(connection_url, pool_pre_ping=True)
-        # # 'sessionmaker' crea sesiones de trabajo individuales
         self.SessionLocal = sessionmaker(bind=self.engine, autocommit=False, autoflush=False)
 
     def get_session(self):
